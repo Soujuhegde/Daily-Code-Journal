1c0c9ee228bc0e3ef11df3ce102c4718f1c3265d
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-20 07:25:58 +0000

    Added i/p o/p typecasting

---
+++ Python_revision/02_i/p&o/p_typecasting.py
@@ -0,0 +1,19 @@
+# Taking input from user
+name = input("Enter your name: ")
+
+# Integer conversion
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age:", age)
+
+# Float conversion
+price = float(input("Enter price: "))
+
+print(price)
+
+# String conversion
+num = str(100)
+
+print(num)
+print(type(num))
