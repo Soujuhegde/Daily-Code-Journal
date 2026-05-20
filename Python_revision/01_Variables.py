9e65b3ab108e155b870a8b213a5f476e6411fa54
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-20 07:24:25 +0000

    added comments

--- Python_revision/01_Variables.py
+++ Python_revision/01_Variables.py
@@ -1,7 +1,21 @@
-name="soujanya"
-age=21
-height=5.0
+# Integer
+age = 21
 
-print(name)
+# Float
+height = 5.2
+
+# String
+name = "Soujanya"
+
+# Boolean
+is_student = True
+
 print(age)
 print(height)
+print(name)
+print(is_student)
+
+# Checking datatype
+print(type(age))
+print(type(height))
+print(type(name))
