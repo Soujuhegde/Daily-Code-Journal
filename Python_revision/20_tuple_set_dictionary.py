1c41fbbf22dd9ea298c93ad4e55a3e111fb9907d
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-20 07:46:04 +0000

    Basic program of tuple,set and dictionary

---
+++ Python_revision/20_tuple_set_dictionary.py
@@ -0,0 +1,25 @@
+#Tuples are immutable 
+data = (10, 20, 30)
+
+print(data[0])
+
+#Set contains unique elements and no duplicate elements
+numbers = {1, 2, 3}
+
+numbers.add(4)
+
+print(numbers)
+
+#Dictionaries are used to store the value in key value pairs 
+
+student = {
+    "name": "Soujanya",
+    "age": 21
+}
+
+print(student["name"])
+
+# Adding new key
+student["course"] = "MCA"
+
+print(student)
