c82e110048f438a7e852f39c9a31f5375e7e203a
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-20 07:29:34 +0000

    Operators added

---
+++ Python_revision/03_operators.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+
+# Arithmetic
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Comparison
+print(a > b)
+print(a == b)
+
+# Logical
+print(a > 5 and b < 10)
+
+# Membership
+name = "soujanya"
+
+print("s" in name)
