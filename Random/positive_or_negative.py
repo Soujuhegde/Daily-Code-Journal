684ccc3864c39d6cf6239123b83c149c34da8993
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-21 16:05:05 +0000

    Positive or Negative

---
+++ Random/positive_or_negative.py
@@ -0,0 +1,12 @@
+# checking positive or negative number
+
+num = int(input("Enter number: "))
+
+if num > 0:
+    print("Positive")
+
+elif num < 0:
+    print("Negative")
+
+else:
+    print("Zero")
