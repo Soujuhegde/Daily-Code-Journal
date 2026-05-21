de12debf9e32247069bbe837dc44e84890132453
Soujanya S P <88967373+Soujuhegde@users.noreply.github.com>
2026-05-21 15:57:34 +0000

    Inheritance basic example

---
+++ OOPS_Concept/Inheritance.py
@@ -0,0 +1,18 @@
+# inheritance example
+
+class Animal:
+
+    def sound(self):
+
+        print("Animal sound")
+
+class Dog(Animal):
+
+    def bark(self):
+
+        print("Dog barks")
+
+d = Dog()
+
+d.sound()
+d.bark()
